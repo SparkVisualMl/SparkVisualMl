74153acee37145976db17f26dea86d5f4a50b48f
master <javalovelinux@163.com>
2018-01-25 04:47:55 -0800

    solve can not scan entity question

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,6 +4,7 @@ import com.spark.model.CustomMultipartResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartResolver;
 @EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
 
 @Configuration
+@EntityScan(basePackages = {"com.spark"})
 @ComponentScan(basePackages = {"com.spark"})
 @ServletComponentScan(basePackages = {"com.spark"})
 @SpringBootApplication
@@ -25,7 +27,6 @@ public class Application {
     public static void main(String [] args){
         SpringApplication.run(Application.class);
     }
-
 
     @Bean(name = "multipartResolver")
     public MultipartResolver customMultipartResolver(){
